9bcd12fe54ba060cc28d270e2ca8019bec2202ea
Alvaro Gramajo <agramajol3@miumg.edu.gt>
2022-05-28 16:17:45 -0600

    Agregue el endpoint para obtener todas las inscripciones y su respectivo metodo en el repositorio.

--- backend/src/main/java/infraestructure/InscripcionRepositoryOracle.java
+++ backend/src/main/java/infraestructure/InscripcionRepositoryOracle.java
@@ -5,13 +5,16 @@
  */
 package infraestructure;
 
+import application.DTO.EstudianteDTO;
 import application.DTO.InscripcionViewDTO;
 import application.repository.IInscripcionRepository;
 import domain.Inscripcion;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,8 +27,8 @@ public class InscripcionRepositoryOracle implements IInscripcionRepository {
 
     @Override
     public boolean registrarInscripcion(Inscripcion inscripcion) {
-        String sql = "INSERT INTO INSCRIPCION(CARNET, NOMBRE, DIRECCION, GENERO, TELEFONO, FECHA_NACIMIENTO, FECHA_INSCRIPCION, FECHA_DECLAMACION, GENERO_POESIA)\n"
-            + "VALUES(?, ?, ?, ?, ?, TO_DATE(?, 'DD/MM/YYYY'), TO_DATE(?, 'DD/MM/YYYY'),TO_DATE(?, 'DD/MM/YYYY'), ?)";
+        String sql = "INSERT INTO INSCRIPCION(CARNET, NOMBRE, DIRECCION, GENERO, TELEFONO, FECHA_NACIMIENTO, FECHA_INSCRIPCION, FECHA_DECLAMACION, GENERO_POESIA, CARRERA)\n"
+            + "VALUES(?, ?, ?, ?, ?, TO_DATE(?, 'DD/MM/YYYY'), TO_DATE(?, 'DD/MM/YYYY'),TO_DATE(?, 'DD/MM/YYYY'), ?, ?)";
         boolean result = false;
         Connection conn = null;
         PreparedStatement ps = null;
@@ -45,6 +48,7 @@ public class InscripcionRepositoryOracle implements IInscripcionRepository {
             ps.setString(7, inscripcion.getFechaInscripcion());
             ps.setString(8, inscripcion.getFechaDeclamacion());
             ps.setString(9, inscripcion.getGeneroPoesia());
+            ps.setString(10, inscripcion.getEstudiante().getCarrera());
             //ResultSet rs = ps.executeQuery();
 
             result = ps.execute();
@@ -72,7 +76,59 @@ public class InscripcionRepositoryOracle implements IInscripcionRepository {
 
     @Override
     public List<InscripcionViewDTO> getAllInscripciones() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT CARNET, NOMBRE, DIRECCION, GENERO, TELEFONO, FECHA_NACIMIENTO, FECHA_INSCRIPCION, FECHA_DECLAMACION, GENERO_POESIA, CARRERA FROM INSCRIPCION";
+        List<InscripcionViewDTO> resultados = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = DriverManager.getConnection(
+                ConfigurationOracle.ORACLE_URL,
+                ConfigurationOracle.ORACLE_USER,
+                ConfigurationOracle.ORACLE_PASSWORD
+            );
+
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                EstudianteDTO eDTO = new EstudianteDTO(
+                    rs.getString("carnet"),
+                    rs.getString("nombre"),
+                    rs.getString("direccion"),
+                    rs.getString("genero"),
+                    rs.getString("fecha_nacimiento"),
+                    rs.getString("carrera"),
+                    rs.getInt("telefono")
+                );
+
+                InscripcionViewDTO ivDTO = new InscripcionViewDTO(eDTO,
+                    rs.getString("genero_poesia"),
+                    rs.getString("fecha_inscripcion"),
+                    rs.getString("fecha_declamacion")
+                );
+
+                resultados.add(ivDTO);
+            }
+        } catch (SQLException sqlException) {
+            Logger.getLogger(InscripcionRepositoryOracle.class.getName())
+                .log(Level.SEVERE, null, sqlException);
+
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(InscripcionRepositoryOracle.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+
+        return resultados;
+
     }
 
 }
